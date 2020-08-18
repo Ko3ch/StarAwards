a377afb7ba8876b08b1702d02afb1fec001782b2
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-18 14:24:36 +0300

    added post form

--- starapp/forms.py
+++ starapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from . models import Rating
+from . models import Rating,Post
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='valid email address.')
@@ -13,4 +13,9 @@ class UserRegisterForm(UserCreationForm):
 class RatingsForm(forms.ModelForm):
     class Meta:
         model = Rating
-        fields = ['design', 'usability', 'content']
+        fields = ('design', 'usability', 'content',)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('photo', 'title', 'url', 'description',)
