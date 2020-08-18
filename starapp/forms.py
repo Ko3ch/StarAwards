1d49106ecff6bb029653546f510ebebc3b57c666
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-18 14:22:10 +0300

    adding rating form

--- starapp/forms.py
+++ starapp/forms.py
@@ -2,9 +2,15 @@ from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from . models import Rating
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='valid email address.')
     class meta:
         model = User
         fields = ('username','email','password1','password2',)
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['design', 'usability', 'content']
