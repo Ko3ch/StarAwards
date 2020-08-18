46f6891d12f6c22a43dc2a139686174bef5a496c
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-18 14:25:41 +0300

    added update user profile form

--- starapp/forms.py
+++ starapp/forms.py
@@ -2,13 +2,18 @@ from django import forms
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from . models import Rating,Post
+from . models import Rating,Post,Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='valid email address.')
     class meta:
         model = User
         fields = ('username','email','password1','password2',)
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'profile_picture', 'bio', 'contact']
 
 class RatingsForm(forms.ModelForm):
     class Meta:
