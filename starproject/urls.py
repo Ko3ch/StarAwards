64156915ecfdc77fe6031929857b95ecba69bfc0
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-20 11:04:26 +0300

    edited url file

--- starproject/urls.py
+++ starproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from django.conf.urls import url
 
 urlpatterns = [
-    path('', include('starapp.urls')),
-    path('admin/', admin.site.urls),
+    url('', include('starapp.urls')),
+    url('admin/', admin.site.urls),
     url('tinymce/', include('tinymce.urls')),
 ]
