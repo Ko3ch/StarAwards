249e4acc82acabbbc83a5d3d7e807f3424566045
tomko3ch@gmail.com <tomko3ch@gmail.com>
2020-08-20 11:43:49 +0300

    edited settings

--- starproject/settings.py
+++ starproject/settings.py
@@ -112,15 +112,6 @@ WSGI_APPLICATION = 'starproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': '***',
-    'USER': '***',
-    'PASSWORD':'****',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
